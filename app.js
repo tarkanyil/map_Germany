55eded663fc1fa6c031515b3bfa8dc16518ab551
Laszlo Tarkanyi <tarkanyil@gmail.com>
2020-10-31 14:33:47 +0100

    Feat: data is loaded daily from RKI API

--- app.js
+++ app.js
@@ -2,30 +2,55 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const fetch = require('node-fetch');
+const schedule = require('node-schedule');
 
 const app = express();
 
-
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-let data = require("./corona2.geo.json");
+let json;
+let dataLoaded = false;
 
-app.get("/", function(req, res){
+let getNewData = async () => {
+  console.log("request started");
+
+  const response = await fetch("https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.geojson");
+
+  if (response.ok) {
+    json = await response.json();
+    console.log("success.");
+    dataLoaded = true;
+  }
+}
+
+//get fresh data when server starts
+getNewData();
+
+//get fresh data every midnight
+schedule.scheduleJob('0 0 * * *', () => {getNewData()});
+
+app.get("/", function (req, res) {
   res.sendFile("index.html");
 });
 
-app.get("/data", function(req, res) {
-  res.send(data);
+app.get("/data", function (req, res) {
+  let tryToSend = setInterval(sendData, 100);
+  function sendData() {
+    if (ready) {
+      console.log(dataLoaded, "kééész");
+      clearInterval(tryToSend);
+      res.json(json);
+    }
+  }
 });
-
 
 let port = process.env.port || 3000;
-app.listen(port, function(){
+app.listen(port, function () {
   console.log("Server started on a good port.");
 });
-
-// Kukucs
